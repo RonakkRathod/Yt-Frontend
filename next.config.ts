9b83ff1db8e1c7608c1690c25ebe75a3fb6ac788
Ronak <ronakrathod0022@gmail.com>
2026-02-27 22:11:07 +0530

    add registries to components.json and update next.config.ts for image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: __dirname,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yt3.ggpht.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
